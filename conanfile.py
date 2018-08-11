04e4f4432aa9d03d4442d2c50374a2ae516f47ff
Uilian Ries <uilianries@gmail.com>
2018-08-11 14:25:36 -0300

    Build with CMake on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
 import os
 import shutil
 
@@ -15,11 +15,14 @@ class LibelfConan(ConanFile):
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "LGPL-2.0"
     exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt", "cmake/CMakeLists.txt"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
+    generators = "cmake"
     autotools = None
     source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Macos":
@@ -34,6 +37,21 @@ class LibelfConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(build_folder=self.build_subfolder)
+        return cmake
+
+    def build_cmake(self):
+        shutil.copyfile(os.path.join("cmake", "CMakeLists.txt"), os.path.join(self.source_subfolder, "CMakeLists.txt"))
+        shutil.move(os.path.join(self.source_subfolder, "lib", "sys_elf.h.w32"), os.path.join(self.source_subfolder, "lib", "sys_elf.h"))
+        cmake = self.configure_cmake()
+        cmake.build()
+
+    def package_cmake(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+
     def configure_autotools(self):
         if not self.autotools:
             args = None
@@ -43,18 +61,30 @@ class LibelfConan(ConanFile):
             self.autotools.configure(configure_dir=self.source_subfolder, args=args)
         return self.autotools
 
-    def build(self):
+    def build_autotools(self):
         autotools = self.configure_autotools()
         autotools.make()
 
-    def package(self):
-        self.copy(pattern="COPYING.LIB", dst="licenses", src=self.source_subfolder)
+    def package_autotools(self):
         autotools = self.configure_autotools()
         autotools.install()
         shutil.rmtree(os.path.join(self.package_folder, "share"), ignore_errors=True)
         shutil.rmtree(os.path.join(self.package_folder, "lib", "locale"), ignore_errors=True)
         if self.settings.os == "Linux" and self.options.shared:
             os.remove(os.path.join(self.package_folder, "lib", "libelf.a"))
+
+    def build(self):
+        if self.settings.os == "Windows":
+            self.build_cmake()
+        else:
+            self.build_autotools()
+
+    def package(self):
+        self.copy(pattern="COPYING.LIB", dst="licenses", src=self.source_subfolder)
+        if self.settings.os == "Windows":
+            self.package_cmake()
+        else:
+            self.package_autotools()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
--- test_package/test_package.c
+++ test_package/test_package.c
@@ -1,24 +1,29 @@
-#include  <err.h>
-#include  <fcntl.h>
-#include  <libelf.h>
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <unistd.h>
 
+#include  <libelf.h>
+
 int main(int argc , char **argv) {
     int fd;
     Elf *e;
-    char *k;
+    const char *k;
     Elf_Kind  ek;
 
-    if (elf_version(EV_CURRENT) ==  EV_NONE)
-        errx(EXIT_FAILURE , "ELF library initialization failed: %s", elf_errmsg ( -1));
+    if (elf_version(EV_CURRENT) ==  EV_NONE) {
+        printf("ELF library initialization failed: %s\n", elf_errmsg ( -1));
+        return EXIT_FAILURE;
+    }
 
-    if ((fd = open(argv[0], O_RDONLY , 0)) < 0)
-        err(EXIT_FAILURE , "open %s failed", argv [0]);
+    if ((fd = open(argv[0], O_RDONLY , 0)) < 0) {
+        printf("open %s failed\n", argv [0]);
+        return EXIT_FAILURE;
+    }
 
-    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
-        errx(EXIT_FAILURE , "elf_begin () failed: %s.", elf_errmsg ( -1));
+    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL) {
+        printf("elf_begin () failed: %s.\n", elf_errmsg ( -1));
+        return EXIT_FAILURE;
+    }
 
     ek = elf_kind(e);
 
@@ -36,9 +41,10 @@ int main(int argc , char **argv) {
             k = "unrecognized";
     }
 
-    (void) printf("%s: %s\n", argv[0], k);
-    (void) elf_end(e);
-    (void) close(fd);
+    printf("%s: %s\n", argv[0], k);
+    elf_end(e);
+
+    close(fd);
 
     return EXIT_SUCCESS;
 }
